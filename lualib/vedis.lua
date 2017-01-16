cea61898b41c04f47f905b82c044e118bf62a9ba
charleeli <717066513@qq.com>
2017-01-16 21:16:54 +0800

    base64

--- lualib/vedis.lua
+++ lualib/vedis.lua
@@ -1,4 +1,5 @@
 local lvedis = require 'lvedis'
+local base64 = require 'base64'
 
 local vedis = {}
 
@@ -14,37 +15,46 @@ end
 
 function vedis.set(key, value)
     lvedis.begin(vedis.db)
-	lvedis.store(vedis.db, key, value)
+	lvedis.store(vedis.db, base64.encode(key), base64.encode(value))
     lvedis.commit(vedis.db)
 end
 
 function vedis.get(key)
-    return lvedis.fetch(vedis.db, key)
+    return base64.decode(lvedis.fetch(vedis.db, base64.encode(key)))
 end
 
 function vedis.hmset(key, row)
-    local cmd = "HMSET "..key.." "
+    local cmd = "HMSET "..base64.encode(key).." "
     for k, v in pairs(row) do
-        cmd = cmd.." "..k.." "..v
+        cmd = cmd.." "..base64.encode(k).." "..base64.encode(v).." "
     end
+	print(cmd)
     lvedis.begin(vedis.db)
     lvedis.exec(vedis.db, cmd)
     lvedis.commit(vedis.db)
 end
 
 function vedis.hgetall(key)
-    local t = lvedis.exec_result_array(vedis.db, "HGETALL "..key)
+	local cmd = "HGETALL "..base64.encode(key)
+    local t = lvedis.exec_result_array(vedis.db, cmd)
 
     local data = {}
     for i=1, #t, 2 do
-        data[t[i]] = t[i+1]
+        data[base64.decode(t[i])] = base64.decode(t[i+1])
     end
 
     return data
 end
 
 function vedis.halls(key)
-    return lvedis.exec_result_array(vedis.db, "HVALS "..key)
+    local t = lvedis.exec_result_array(vedis.db, "HVALS "..base64.encode(key))
+	
+	local data = {}
+    for i=1, #t do
+        table.insert(data, base64.decode(t[i]))
+    end
+
+    return data
 end
 
 return vedis
